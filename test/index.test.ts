4bf7870f8430060a5aab6e477fbde992e6d5bfda
ntnyq <ntnyq13@gmail.com>
2022-11-29 13:55:26 +0800

    test: add type test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,11 @@
-import { describe, expect, it } from 'vitest'
+import {
+  assertType,
+  describe,
+  expect,
+  expectTypeOf,
+  it,
+} from 'vitest'
+import type { PkgDepType } from '../src'
 import {
   configs,
   createDepValidator,
@@ -156,5 +163,29 @@ describe(`configs`, () => {
         "vite.config.mts",
       ]
     `)
+  })
+})
+
+describe(`typecheck`, () => {
+  it(`should return type match`, () => {
+    expectTypeOf(hasVite).returns.toBeBoolean()
+    expectTypeOf(hasViteConfig).returns.toBeBoolean()
+    expectTypeOf(hasViteDep).returns.toBeBoolean()
+    expectTypeOf(hasViteDevDep).returns.toBeBoolean()
+    expectTypeOf(hasVitePeerDep).returns.toBeBoolean()
+    expectTypeOf(hasViteInPkg).returns.toBeBoolean()
+    expectTypeOf(createDepValidator).returns.toBeFunction()
+    expectTypeOf(readPkg).returns.toBeObject()
+  })
+
+  it(`should params type match`, () => {
+    assertType<(cwd?: string) => boolean>(hasVite)
+    assertType<(cwd?: string) => boolean>(hasViteConfig)
+    assertType<(cwd?: string) => boolean>(hasViteDep)
+    assertType<(cwd?: string) => boolean>(hasViteDevDep)
+    assertType<(cwd?: string) => boolean>(hasVitePeerDep)
+    assertType<(cwd?: string) => boolean>(hasViteInPkg)
+    assertType<(pkg: PkgDepType) => (cwd: string) => boolean>(createDepValidator)
+    assertType<(cwd?: string) => any>(readPkg)
   })
 })
